b38ff3350bac4f0241f55ce68cd0d947274981d0
Kai Meng <Kaimeng44@proton.me>
2026-04-14 02:04:45 -0700

    teddy bear ability added

--- GDIM 61 Game Project/Assets/Scripts/Petsitter.cs
+++ GDIM 61 Game Project/Assets/Scripts/Petsitter.cs
@@ -21,6 +21,31 @@ public class Petsitter : Pet
     {
         WhoAndWhere();
         
+        if (current_position == 0 && team_list.Count > 1) // if teddy bear is at the front
+        {
+            team_list[1].GetComponent<Pet>().healthPoints += 1;
+        }
+        
+        else if (current_position == team_list.Count - 1) // if teddy bear is at the back
+        {
+            team_list[current_position - 1].GetComponent<Pet>().healthPoints += 1;
+        }
+
+        else if (team_list.Count > 2) // if teddy bear is somewhere in between
+        {
+            if (team_list[current_position + 1].GetComponent<Pet>().healthPoints > team_list[current_position - 1].GetComponent<Pet>().healthPoints)
+            {
+                team_list[current_position + 1].GetComponent<Pet>().healthPoints += 1; // heals pet in behind
+            }
+            else
+            {
+                team_list[current_position - 1].GetComponent<Pet>().healthPoints += 1; // heals pet in front
+            }
+
+
+        }
+
+            
 
 
 
