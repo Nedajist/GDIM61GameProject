b5678c4479702c2f79e0c476541b29ea0c50db3d
nathannnhernandez <personalhernandez17@gmail.com>
2026-05-04 12:08:41 -0700

    all pet abilities

---
+++ GDIM 61 Game Project/Assets/Scripts/Specific Pet Scripts/SuperShark.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuperShark : Pet
+{
+    public override void FaceLeft()
+    {
+        _sprite.flipX = true;
+    }
+
+    public override void FaceRight()
+    {
+        _sprite.flipX = false;
+    }
+    protected override void OnCollisionEnter2D(Collision2D collision)
+    {
+        base.OnCollisionEnter2D(collision);
+
+        Vector2 lineToCollider = collision.contacts[0].point - (Vector2) transform.position;
+        Pet collidingPet = collision.transform.GetComponent<Pet>();
+        float approaching = Vector2.Dot(lineToCollider, collision.relativeVelocity); 
+
+        if (collidingPet.petSide != petSide && approaching >= 0)
+        {
+            SmellBlood(collidingPet);
+        }
+    }
+    void SmellBlood(Pet whoDidIRunInto)
+    {
+        GameController.instance.CullLists(enemyList);
+        Vector2 randomVector2 = whoDidIRunInto.transform.position - transform.position;
+        randomVector2 = randomVector2.normalized;
+        _rb.velocity = (randomVector2 * speed * speedMultiplier);
+    }
+}
---
+++ GDIM 61 Game Project/Assets/Scripts/Specific Pet Scripts/VampyreBat.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : Pet
+{
+    public override void FaceLeft()
+    {
+        _sprite.flipX = false;
+    }
+
+    public override void FaceRight()
+    {
+        _sprite.flipX = true;
+    }
+    protected override void OnCollisionEnter2D(Collision2D collision)
+    {
+        base.OnCollisionEnter2D(collision);
+
+        Vector2 lineToCollider = collision.contacts[0].point - (Vector2) transform.position;
+        Pet collidingPet = collision.transform.GetComponent<Pet>();
+        float approaching = Vector2.Dot(lineToCollider, collision.relativeVelocity); 
+
+        if (collidingPet.petSide != petSide && approaching >= 0)
+        {
+            healthPoints += attack;
+        }
+    }
+}
