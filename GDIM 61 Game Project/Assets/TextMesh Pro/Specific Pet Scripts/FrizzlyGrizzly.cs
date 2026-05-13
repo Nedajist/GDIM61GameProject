38e31f2850ad8c1a7116b68c1306b628060d330b
nathannnhernandez <personalhernandez17@gmail.com>
2026-05-12 18:58:01 -0700

    grizzly abilituy

---
+++ GDIM 61 Game Project/Assets/TextMesh Pro/Specific Pet Scripts/FrizzlyGrizzly.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrizzlyGrizzly : Pet
+{
+    [SerializeField] private SpriteRenderer spriteRender;
+    [SerializeField] private float growlDelay = 5f;
+    [SerializeField] private GameObject growlOnam;
+    //Onam position modifiers
+            [SerializeField] private float xOnamPosModifier;
+            [SerializeField] private float yOnamPosModifier;
+    [SerializeField] private float growlForce = 10f;
+    //[SerializeField] private GameObject growlCollider;
+    private Rigidbody2D rb;
+    private TeddyBear teddyBear;
+
+    private Pet pets;
+    private List<GameObject> allyList;
+    public override void FaceLeft()
+    {
+        _sprite.flipX = true;
+    }
+
+    public override void FaceRight()
+    {
+        _sprite.flipX = false;
+    }
+    protected override void Start()
+    {
+        base.Start();
+        allyList = pets.teamList;
+    }
+    void Update()
+    {
+        if (teddyBear.isCubAlive == false)
+        {
+            RagePhase();
+            Growl(growlDelay);
+        }
+    }
+    void RagePhase()
+    {
+        spriteRender.color = Color.red;
+    }
+    IEnumerator Growl(float cooldown)
+    {
+        yield return new WaitForSeconds(cooldown);
+
+        Instantiate(growlOnam, transform);       
+        foreach (GameObject pets in allyList)
+        {
+            Rigidbody2D rb = pets.GetComponent<Rigidbody2D>();
+            if (pets.transform.position.y < transform.position.y)
+            {
+                growlForce *= -1;
+            }
+            rb.AddForce(transform.up * growlForce);
+        }
+    }
+}
