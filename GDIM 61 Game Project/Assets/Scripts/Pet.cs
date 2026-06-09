165558d92ebf47c8c4709f3fb867b64b2e39727c
nathannnhernandez <personalhernandez17@gmail.com>
2026-06-09 12:39:59 -0700

    mystery

--- GDIM 61 Game Project/Assets/Scripts/Pet.cs
+++ GDIM 61 Game Project/Assets/Scripts/Pet.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Threading;
 using Unity.VisualScripting;
-using Unity.VisualScripting.Dependencies.Sqlite;
 using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
 using UnityEngine.U2D;
